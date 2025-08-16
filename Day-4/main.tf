b6a0f1dd95927a13c36dd6474abcddd4c1aa3764
Saikumar-2398 <katamsaikumar001@gmail.com>
2025-08-16 14:31:09 +0530

    Day-4

---
+++ Day-4/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "name" {
+    ami = "ami-0d0ad8bb301edb745"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "test_ec2"
+    }
+  
+}
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "test_vpc"
+    }
+  
+}
